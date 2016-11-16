633a56fb17821895a9181fbedeada87a0475ec78
Jake Weinstein <xboxlover360@gmail.com>
2016-11-16 22:49:12 +0100

    Revert "sepolicy: Exclude for AOSP device builds"
    * Nexus devices will be using this as well.
    
    This reverts commit 8b608a4b8ae07abbe62264f69089c00b1f3e2b60.
    
    Change-Id: I9b4111f67db6211459d165ef6a8661d702f86b43

--- Android.mk
+++ Android.mk
@@ -1,5 +1,4 @@
 # Board specific SELinux policy variable definitions
-ifneq ($(TARGET_USES_AOSP),true)
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 LOCAL_PATH:= $(call my-dir)
 BOARD_SEPOLICY_DIRS := \
@@ -9,5 +8,4 @@ BOARD_SEPOLICY_DIRS := \
        $(LOCAL_PATH)/test \
        $(LOCAL_PATH)/$(TARGET_BOARD_PLATFORM)
 
-endif
 endif
