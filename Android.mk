5e919f4c4393c2e8f186afa79175826cf4316be6
TheStrix <parthbhatia98@gmail.com>
2019-03-31 15:34:23 +0530

    sepolicy: Include vendor sepolicies only if vendor image is built
    Devices using prebuilt vendor images should be able to skip
    vendor sepolicy rules.
    
    Change-Id: I08ebc9471dba202bec10a76d09fd9e26066efe6f

--- Android.mk
+++ Android.mk
@@ -1,6 +1,9 @@
 # Board specific SELinux policy variable definitions
 ifneq ($(TARGET_EXCLUDE_QCOM_SEPOLICY),true)
 ifeq ($(call is-vendor-board-platform,QCOM),true)
+
+# Include vendor sepolicies only if vendor image is built.
+ifdef BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE
 LOCAL_PATH:= $(call my-dir)
 BOARD_SEPOLICY_DIRS := \
        $(BOARD_SEPOLICY_DIRS) \
@@ -23,6 +26,7 @@ endif
 ifneq (,$(filter 24 25 26 27, $(PRODUCT_SHIPPING_API_LEVEL)))
 BOARD_SEPOLICY_DIRS += \
        $(LOCAL_PATH)/vendor/ota/$(TARGET_BOARD_PLATFORM)
+endif
 endif
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
