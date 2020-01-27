9273153a3f601f5f640ecfb6a849f21d39f2e47d
Jake Weinstein <jake@aospa.co>
2020-01-27 02:36:18 +0100

    sepolicy: modify Makefiles so the correct SEPolicy can be chosen.
    Use default QC sepolicy unless any of the following conditions are met:
    1. Not a QCOM board
    2. TARGET_EXCLUDE_QCOM_SEPOLICY is true
    3. TARGET_USES_QCOM_LEGACY_UM_SEPOLICY is true (opted in by default
       for legacy UM chipsets)
    4. TARGET_USES_QCOM_LEGACY_NON_UM_SEPOLICY is true (opted in by default
       for legacy non-UM chipsets)
    
    - Use LOCAL_PATH
    - Remove calls to LOS SEPolicy since we call ours in build system
    
    Change-Id: I7de4499151a19c94849c7d0ef4a7830e3e0587ad

--- legacy-non-um/Android.mk
+++ legacy-non-um/Android.mk
@@ -1,10 +1,31 @@
-# Don't recurse into the platform makefiles. We don't care about them, and
-# we don't want to force a reset of BOARD_SEPOLICY_DIRS
-#
-# If you want to use these policies, add a
-#
-# include device/qcom/sepolicy-legacy/sepolicy.mk
-#
-# to your device's BoardConfig. It is highly recommended that in case
-# you have your own BOARD_SEPOLICY_DIRS declaration,
-# the inclusion happens _before_ those lines
+ifeq ($(TARGET_USES_QCOM_LEGACY_NON_UM_SEPOLICY),true)
+
+LOCAL_PATH:= $(call my-dir)
+
+# Board specific SELinux policy variable definitions
+BOARD_SEPOLICY_DIRS += \
+    $(LOCAL_PATH)/common \
+    $(LOCAL_PATH)/ssg \
+    $(LOCAL_PATH)/$(TARGET_BOARD_PLATFORM)
+
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+BOARD_SEPOLICY_DIRS += \
+    $(LOCAL_PATH)/test
+
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    $(LOCAL_PATH)/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    $(LOCAL_PATH)/private
+
+# Board specific SELinux policy variable definitions for legacy devices
+BOARD_SEPOLICY_DIRS += \
+    $(LOCAL_PATH)/legacy-common
+
+# Add sepolicy version to support OS upgrade and backward compatibility
+BOARD_SEPOLICY_VERS := $(PLATFORM_SDK_VERSION).0
+
+endif
