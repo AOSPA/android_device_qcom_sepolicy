4267c16c312e407a3ee98c92f0565b6e69acdf24
Anay Wadhera <awadhera@berkeley.edu>
2020-02-21 19:26:36 -0800

    sepolicy: determine legacy/non-legacy properly
    Change-Id: I364870fadc3c160a077847681bcac7d532a907f6

--- Android.mk
+++ Android.mk
@@ -11,83 +11,81 @@ endif
 # Board specific SELinux policy variable definitions
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 ifneq ($(TARGET_EXCLUDE_QCOM_SEPOLICY),true)
-ifneq ($(TARGET_USES_QCOM_LEGACY_UM_SEPOLICY),true)
-ifneq ($(TARGET_USES_QCOM_LEGACY_NON_UM_SEPOLICY),true)
-LOCAL_PATH:= $(call my-dir)
+# Include Legacy Makefiles
+ifeq ($(TARGET_USES_QCOM_LEGACY_NON_UM_SEPOLICY),true)
+    include device/qcom/sepolicy/legacy-non-um/legacy-non-um.mk
+else ifeq ($(TARGET_USES_QCOM_LEGACY_UM_SEPOLICY),true)
+    include device/qcom/sepolicy/legacy-um/legacy-um.mk
+else
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
     $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
-    $(LOCAL_PATH)/generic/public
+    device/qcom/sepolicy/generic/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
     $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
-    $(LOCAL_PATH)/generic/private
+    device/qcom/sepolicy/generic/private
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
     $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
-    $(LOCAL_PATH)/qva/public
+    device/qcom/sepolicy/qva/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
     $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
-    $(LOCAL_PATH)/qva/private
+    device/qcom/sepolicy/qva/private
 
 # sepolicy rules for product images
 PRODUCT_PUBLIC_SEPOLICY_DIRS := \
     $(PRODUCT_PUBLIC_SEPOLICY_DIRS) \
-     $(LOCAL_PATH)/product/public
+     device/qcom/sepolicy/product/public
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS := \
     $(PRODUCT_PRIVATE_SEPOLICY_DIRS) \
-     $(LOCAL_PATH)/product/private
+     device/qcom/sepolicy/product/private
 
 ifeq (,$(filter sdm845 sdm710 qcs605, $(TARGET_BOARD_PLATFORM)))
     BOARD_SEPOLICY_DIRS := \
        $(BOARD_SEPOLICY_DIRS) \
-       $(LOCAL_PATH) \
-       $(LOCAL_PATH)/generic/vendor/common \
-       $(LOCAL_PATH)/qva/vendor/common/sysmonapp \
-       $(LOCAL_PATH)/qva/vendor/ssg \
-       $(LOCAL_PATH)/timeservice \
-       $(LOCAL_PATH)/qva/vendor/qwesas \
-       $(LOCAL_PATH)/qva/vendor/common
+       device/qcom/sepolicy \
+       device/qcom/sepolicy/generic/vendor/common \
+       device/qcom/sepolicy/qva/vendor/common/sysmonapp \
+       device/qcom/sepolicy/qva/vendor/ssg \
+       device/qcom/sepolicy/timeservice \
+       device/qcom/sepolicy/qva/vendor/qwesas \
+       device/qcom/sepolicy/qva/vendor/common
 
     ifeq ($(TARGET_SEPOLICY_DIR),)
-      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/generic/vendor/$(TARGET_BOARD_PLATFORM)
-      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/qva/vendor/$(TARGET_BOARD_PLATFORM)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/generic/vendor/$(TARGET_BOARD_PLATFORM)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/qva/vendor/$(TARGET_BOARD_PLATFORM)
     else
-      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/generic/vendor/$(TARGET_SEPOLICY_DIR)
-      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/qva/vendor/$(TARGET_SEPOLICY_DIR)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/generic/vendor/$(TARGET_SEPOLICY_DIR)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/qva/vendor/$(TARGET_SEPOLICY_DIR)
     endif
 
     ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-    BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/generic/vendor/test
-    BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/qva/vendor/test
+    BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/generic/vendor/test
+    BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/qva/vendor/test
     endif
 endif
 
 ifneq (,$(filter sdm845 sdm710 qcs605, $(TARGET_BOARD_PLATFORM)))
     BOARD_SEPOLICY_DIRS := \
                  $(BOARD_SEPOLICY_DIRS) \
-                 $(LOCAL_PATH) \
-                 $(LOCAL_PATH)/legacy/vendor/common/sysmonapp \
-                 $(LOCAL_PATH)/legacy/vendor/ssg \
-                 $(LOCAL_PATH)/timeservice \
-                 $(LOCAL_PATH)/legacy/vendor/common
+                 device/qcom/sepolicy \
+                 device/qcom/sepolicy/legacy/vendor/common/sysmonapp \
+                 device/qcom/sepolicy/legacy/vendor/ssg \
+                 device/qcom/sepolicy/timeservice \
+                 device/qcom/sepolicy/legacy/vendor/common
 
     ifeq ($(TARGET_SEPOLICY_DIR),)
-      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/legacy/vendor/$(TARGET_BOARD_PLATFORM)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy/vendor/$(TARGET_BOARD_PLATFORM)
     else
-      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/legacy/vendor/$(TARGET_SEPOLICY_DIR)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy/vendor/$(TARGET_SEPOLICY_DIR)
     endif
     ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-    BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/legacy/vendor/test
+    BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy/vendor/test
     endif
 endif # sdm845 sdm710 qcs605
-
-endif # Legacy UM SEPolicy
-endif # Legacy Non UM SEPolicy
-
-# Include Legacy Makefiles
-include $(call all-subdir-makefiles)
+endif
 
 endif # Exclude QCOM SEPolicy
 endif # QCOM Platform
--- legacy-non-um/legacy-non-um.mk
+++ legacy-non-um/legacy-non-um.mk
@@ -1,29 +1,25 @@
 ifeq ($(TARGET_USES_QCOM_LEGACY_NON_UM_SEPOLICY),true)
 
-LOCAL_PATH:= $(call my-dir)
-
 # Board specific SELinux policy variable definitions
 BOARD_SEPOLICY_DIRS += \
-    $(LOCAL_PATH)/common \
-    $(LOCAL_PATH)/ssg \
-    $(LOCAL_PATH)/$(TARGET_BOARD_PLATFORM)
+    device/qcom/sepolicy/legacy-non-um/common \
+    device/qcom/sepolicy/legacy-non-um/ssg \
+    device/qcom/sepolicy/legacy-non-um/$(TARGET_BOARD_PLATFORM)
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 BOARD_SEPOLICY_DIRS += \
-    $(LOCAL_PATH)/test
-
-SELINUX_IGNORE_NEVERALLOWS := true
+    device/qcom/sepolicy/legacy-non-um/test
 endif
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    $(LOCAL_PATH)/public
+    device/qcom/sepolicy/legacy-non-um/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    $(LOCAL_PATH)/private
+    device/qcom/sepolicy/legacy-non-um/private
 
 # Board specific SELinux policy variable definitions for legacy devices
 BOARD_SEPOLICY_DIRS += \
-    $(LOCAL_PATH)/legacy-common
+    device/qcom/sepolicy/legacy-non-um/legacy-common
 
 # Add sepolicy version to support OS upgrade and backward compatibility
 BOARD_SEPOLICY_VERS := $(PLATFORM_SDK_VERSION).0
