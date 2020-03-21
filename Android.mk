4bb48739b225b6245acc2164f330483889a0957e
Jake Weinstein <jake@aospa.co>
2020-03-21 03:30:48 +0100

    sepolicy: remove legacy SEPolicies
    We don't need these anymore.
    
    Change-Id: Iff8e5b89f337a22a864fe3dafed59d7ce9d94bf0

--- Android.mk
+++ Android.mk
@@ -1,91 +1,74 @@
-# Use QCOM Legacy UM SEPolicy by default for legacy UM boards
-ifeq ($(call is-board-platform-in-list, msm8909 msm8937 msm8953 msm8996 msm8998 sdm660),true)
-  TARGET_USES_QCOM_LEGACY_UM_SEPOLICY ?= true
-endif
-
-# Use QCOM Legacy SEPolicy by default for legacy pre-UM boards
-ifeq ($(call is-board-platform-in-list, apq8084 msm8226 msm8909 msm8916 msm8952 msm8960 msm8974 msm8976 msm8992 msm8994),true)
-  TARGET_USES_QCOM_LEGACY_SEPOLICY ?= true
-endif
-
 # Board specific SELinux policy variable definitions
-ifeq ($(call is-vendor-board-platform,QCOM),true)
 ifneq ($(TARGET_EXCLUDE_QCOM_SEPOLICY),true)
-# Include Legacy Makefiles
-ifeq ($(TARGET_USES_QCOM_LEGACY_NON_UM_SEPOLICY),true)
-    include device/qcom/sepolicy/legacy-non-um/legacy-non-um.mk
-else ifeq ($(TARGET_USES_QCOM_LEGACY_UM_SEPOLICY),true)
-    include device/qcom/sepolicy/legacy-um/legacy-um.mk
-else
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+LOCAL_PATH:= $(call my-dir)
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
     $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
-    device/qcom/sepolicy/generic/public
+    $(LOCAL_PATH)/generic/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
     $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
-    device/qcom/sepolicy/generic/private
+    $(LOCAL_PATH)/generic/private
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
     $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
-    device/qcom/sepolicy/qva/public
+    $(LOCAL_PATH)/qva/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
     $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
-    device/qcom/sepolicy/qva/private
+    $(LOCAL_PATH)/qva/private
 
 # sepolicy rules for product images
 PRODUCT_PUBLIC_SEPOLICY_DIRS := \
     $(PRODUCT_PUBLIC_SEPOLICY_DIRS) \
-     device/qcom/sepolicy/product/public
+     $(LOCAL_PATH)/product/public
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS := \
     $(PRODUCT_PRIVATE_SEPOLICY_DIRS) \
-     device/qcom/sepolicy/product/private
+     $(LOCAL_PATH)/product/private
 
 ifeq (,$(filter sdm845 sdm710 qcs605, $(TARGET_BOARD_PLATFORM)))
     BOARD_SEPOLICY_DIRS := \
        $(BOARD_SEPOLICY_DIRS) \
-       device/qcom/sepolicy \
-       device/qcom/sepolicy/generic/vendor/common \
-       device/qcom/sepolicy/qva/vendor/common/sysmonapp \
-       device/qcom/sepolicy/qva/vendor/ssg \
-       device/qcom/sepolicy/timeservice \
-       device/qcom/sepolicy/qva/vendor/qwesas \
-       device/qcom/sepolicy/qva/vendor/common
+       $(LOCAL_PATH) \
+       $(LOCAL_PATH)/generic/vendor/common \
+       $(LOCAL_PATH)/qva/vendor/common/sysmonapp \
+       $(LOCAL_PATH)/qva/vendor/ssg \
+       $(LOCAL_PATH)/timeservice \
+       $(LOCAL_PATH)/qva/vendor/qwesas \
+       $(LOCAL_PATH)/qva/vendor/common
 
     ifeq ($(TARGET_SEPOLICY_DIR),)
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/generic/vendor/$(TARGET_BOARD_PLATFORM)
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/qva/vendor/$(TARGET_BOARD_PLATFORM)
+      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/generic/vendor/$(TARGET_BOARD_PLATFORM)
+      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/qva/vendor/$(TARGET_BOARD_PLATFORM)
     else
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/generic/vendor/$(TARGET_SEPOLICY_DIR)
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/qva/vendor/$(TARGET_SEPOLICY_DIR)
+      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/generic/vendor/$(TARGET_SEPOLICY_DIR)
+      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/qva/vendor/$(TARGET_SEPOLICY_DIR)
     endif
 
     ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-    BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/generic/vendor/test
-    BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/qva/vendor/test
+    BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/generic/vendor/test
+    BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/qva/vendor/test
     endif
 endif
 
 ifneq (,$(filter sdm845 sdm710 qcs605, $(TARGET_BOARD_PLATFORM)))
     BOARD_SEPOLICY_DIRS := \
                  $(BOARD_SEPOLICY_DIRS) \
-                 device/qcom/sepolicy \
-                 device/qcom/sepolicy/legacy/vendor/common/sysmonapp \
-                 device/qcom/sepolicy/legacy/vendor/ssg \
-                 device/qcom/sepolicy/timeservice \
-                 device/qcom/sepolicy/legacy/vendor/common
+                 $(LOCAL_PATH) \
+                 $(LOCAL_PATH)/legacy/vendor/common/sysmonapp \
+                 $(LOCAL_PATH)/legacy/vendor/ssg \
+                 $(LOCAL_PATH)/timeservice \
+                 $(LOCAL_PATH)/legacy/vendor/common
 
     ifeq ($(TARGET_SEPOLICY_DIR),)
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy/vendor/$(TARGET_BOARD_PLATFORM)
+      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/legacy/vendor/$(TARGET_BOARD_PLATFORM)
     else
-      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy/vendor/$(TARGET_SEPOLICY_DIR)
+      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/legacy/vendor/$(TARGET_SEPOLICY_DIR)
     endif
     ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-    BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy/vendor/test
+    BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/legacy/vendor/test
     endif
-endif # sdm845 sdm710 qcs605
 endif
-
-endif # Exclude QCOM SEPolicy
-endif # QCOM Platform
+endif
+endif
