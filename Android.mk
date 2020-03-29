06de5fd39da8e228a2958028ffa1888772f1d136
Arian <arian.kulmer@web.de>
2020-03-29 10:46:11 +0200

    Fixup pre-UM sepolicy inclusion and move to legacy-pre-um
    Change-Id: I7f20e1f3a8c83c40d4e669a49accf2d61bbbc8b6

--- Android.mk
+++ Android.mk
@@ -5,15 +5,15 @@ endif
 
 # Use QCOM Legacy SEPolicy by default for legacy pre-UM boards
 ifeq ($(call is-board-platform-in-list, apq8084 msm8226 msm8909 msm8916 msm8952 msm8960 msm8974 msm8976 msm8992 msm8994),true)
-  TARGET_USES_QCOM_LEGACY_SEPOLICY ?= true
+  TARGET_USES_QCOM_LEGACY_PRE_UM_SEPOLICY ?= true
 endif
 
 # Board specific SELinux policy variable definitions
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 ifneq ($(TARGET_EXCLUDE_QCOM_SEPOLICY),true)
 # Include Legacy Makefiles
-ifeq ($(TARGET_USES_QCOM_LEGACY_NON_UM_SEPOLICY),true)
-    include device/qcom/sepolicy/legacy-non-um/legacy-non-um.mk
+ifeq ($(TARGET_USES_QCOM_LEGACY_PRE_UM_SEPOLICY),true)
+    include device/qcom/sepolicy/legacy-pre-um/legacy-pre-um.mk
 else ifeq ($(TARGET_USES_QCOM_LEGACY_UM_SEPOLICY),true)
     include device/qcom/sepolicy/legacy-um/legacy-um.mk
 else
--- legacy-pre-um/legacy-pre-um.mk
+++ legacy-pre-um/legacy-pre-um.mk
@@ -1,28 +1,28 @@
-ifeq ($(TARGET_USES_QCOM_LEGACY_NON_UM_SEPOLICY),true)
+ifeq ($(TARGET_USES_QCOM_LEGACY_PRE_UM_SEPOLICY),true)
 
 # Board specific SELinux policy variable definitions
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/qcom/sepolicy/legacy-non-um/common \
-    device/qcom/sepolicy/legacy-non-um/ssg \
-    device/qcom/sepolicy/legacy-non-um/$(TARGET_BOARD_PLATFORM)
+    device/qcom/sepolicy/legacy-pre-um/common \
+    device/qcom/sepolicy/legacy-pre-um/ssg \
+    device/qcom/sepolicy/legacy-pre-um/$(TARGET_BOARD_PLATFORM)
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 BOARD_SEPOLICY_DIRS += \
-    device/qcom/sepolicy/legacy-non-um/test
+    device/qcom/sepolicy/legacy-pre-um/test
 endif # Userdebug or Eng
 endif # !PREBUILT_VENDOR_SEPOLICY
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy/legacy-non-um/public
+    device/qcom/sepolicy/legacy-pre-um/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/qcom/sepolicy/legacy-non-um/private
+    device/qcom/sepolicy/legacy-pre-um/private
 
 # Board specific SELinux policy variable definitions for legacy devices
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/qcom/sepolicy/legacy-non-um/legacy-common
+    device/qcom/sepolicy/legacy-pre-um/legacy-common
 endif
 
 # Add sepolicy version to support OS upgrade and backward compatibility
