d9ad1a729d17c333f485385d5e1ba1bae2d14a0b
Arian <arian.kulmer@web.de>
2020-03-31 19:23:09 +0200

    sepolicy: Cleanup makefiles
    * Take use of the += feature
    * Fix some whitespaces
    
    Change-Id: I90bab90dc796369bc34d56a3473a4b439b7d979e

--- legacy-um/legacy-um.mk
+++ legacy-um/legacy-um.mk
@@ -1,27 +1,27 @@
 # Board specific SELinux policy variable definitions
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
-BOARD_SEPOLICY_DIRS := \
-    device/qcom/sepolicy/legacy-um/vendor/common \
-    device/qcom/sepolicy/legacy-um/vendor/ssg \
-    device/qcom/sepolicy/legacy-um/timeservice \
-    device/qcom/sepolicy/legacy-um/vendor/common/sysmonapp \
-    $(BOARD_SEPOLICY_DIRS)
+    BOARD_SEPOLICY_DIRS := \
+        device/qcom/sepolicy/legacy-um/vendor/common \
+        device/qcom/sepolicy/legacy-um/vendor/ssg \
+        device/qcom/sepolicy/legacy-um/timeservice \
+        device/qcom/sepolicy/legacy-um/vendor/common/sysmonapp \
+        $(BOARD_SEPOLICY_DIRS)
 
-ifeq ($(TARGET_SEPOLICY_DIR),)
-BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy-um/vendor/$(TARGET_BOARD_PLATFORM)
-else
-BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy-um/vendor/$(TARGET_SEPOLICY_DIR)
-endif
+    ifeq ($(TARGET_SEPOLICY_DIR),)
+        BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy-um/vendor/$(TARGET_BOARD_PLATFORM)
+    else
+        BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy-um/vendor/$(TARGET_SEPOLICY_DIR)
+    endif
 
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-BOARD_SEPOLICY_DIRS += \
-    device/qcom/sepolicy/legacy-um/vendor/test
-endif
+    ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+        BOARD_SEPOLICY_DIRS += \
+            device/qcom/sepolicy/legacy-um/vendor/test
+    endif
 
-ifneq (,$(filter 24 25 26 27, $(PRODUCT_SHIPPING_API_LEVEL)))
-BOARD_SEPOLICY_DIRS += \
-    device/qcom/sepolicy/legacy-um/vendor/ota/$(TARGET_BOARD_PLATFORM)
-endif # Launched with Legacy API
+    ifneq (,$(filter 24 25 26 27, $(PRODUCT_SHIPPING_API_LEVEL)))
+        BOARD_SEPOLICY_DIRS += \
+            device/qcom/sepolicy/legacy-um/vendor/ota/$(TARGET_BOARD_PLATFORM)
+    endif # Launched with Legacy API
 endif # !PREBUILT_VENDOR_SEPOLICY
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
