c109d2577164236f8c8e83d1d735cf3d15937571
Arian <arian.kulmer@web.de>
2020-03-31 19:23:09 +0200

    sepolicy: Cleanup makefiles
    * Take use of the += feature
    * Fix some whitespaces
    
    Change-Id: I90bab90dc796369bc34d56a3473a4b439b7d979e

--- legacy-um/legacy-um.mk
+++ legacy-um/legacy-um.mk
@@ -1,25 +1,24 @@
 # Board specific SELinux policy variable definitions
-BOARD_SEPOLICY_DIRS := \
+BOARD_SEPOLICY_DIRS += \
     device/qcom/sepolicy/legacy-um/vendor/common \
     device/qcom/sepolicy/legacy-um/vendor/ssg \
     device/qcom/sepolicy/legacy-um/timeservice \
-    device/qcom/sepolicy/legacy-um/vendor/common/sysmonapp \
-    $(BOARD_SEPOLICY_DIRS)
+    device/qcom/sepolicy/legacy-um/vendor/common/sysmonapp
 
 ifeq ($(TARGET_SEPOLICY_DIR),)
-BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy-um/vendor/$(TARGET_BOARD_PLATFORM)
+    BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy-um/vendor/$(TARGET_BOARD_PLATFORM)
 else
-BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy-um/vendor/$(TARGET_SEPOLICY_DIR)
+    BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy-um/vendor/$(TARGET_SEPOLICY_DIR)
 endif
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-BOARD_SEPOLICY_DIRS += \
-    device/qcom/sepolicy/legacy-um/vendor/test
+    BOARD_SEPOLICY_DIRS += \
+        device/qcom/sepolicy/legacy-um/vendor/test
 endif
 
 ifneq (,$(filter 24 25 26 27, $(PRODUCT_SHIPPING_API_LEVEL)))
-BOARD_SEPOLICY_DIRS += \
-    device/qcom/sepolicy/legacy-um/vendor/ota/$(TARGET_BOARD_PLATFORM)
+    BOARD_SEPOLICY_DIRS += \
+        device/qcom/sepolicy/legacy-um/vendor/ota/$(TARGET_BOARD_PLATFORM)
 endif
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
