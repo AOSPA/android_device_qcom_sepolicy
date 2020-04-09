4ac1f587ce05fd95475a0a7bd2115f34de13d536
Jake Weinstein <jake@aospa.co>
2020-04-09 21:50:38 +0200

    sepolicy: Include vendor sepolicies only if vendor image is built
    Devices using prebuilt vendor images should be able to skip
    vendor sepolicy rules.
    
    Change-Id: I08ebc9471dba202bec10a76d09fd9e26066efe6f

--- legacy-um/legacy-um.mk
+++ legacy-um/legacy-um.mk
@@ -1,4 +1,5 @@
 # Board specific SELinux policy variable definitions
+ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS := \
     device/qcom/sepolicy/legacy-um/vendor/common \
     device/qcom/sepolicy/legacy-um/vendor/ssg \
@@ -20,7 +21,8 @@ endif
 ifneq (,$(filter 24 25 26 27, $(PRODUCT_SHIPPING_API_LEVEL)))
 BOARD_SEPOLICY_DIRS += \
     device/qcom/sepolicy/legacy-um/vendor/ota/$(TARGET_BOARD_PLATFORM)
-endif
+endif # Launched with Legacy API
+endif # !PREBUILT_VENDOR_SEPOLICY
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
     device/qcom/sepolicy/legacy-um/public
